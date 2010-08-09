7e434a604a6c1c82b73708cdd9b497024729f2f6
Andreas Wuerl <andi@donar>
2010-08-09 06:27:17 +0000

    cleanup of lib/network/Base

---
+++ source/lib/network/Queue.h
@@ -0,0 +1,41 @@
+#ifndef BLITZORTUNG_NETWORK_QUEUE_H_
+#define BLITZORTUNG_NETWORK_QUEUE_H_
+
+#include <queue>
+
+#include <boost/thread/thread.hpp>
+
+namespace blitzortung {
+  namespace network {
+
+    template <typename T>
+      class Queue {
+	private:
+	  int timeout_;
+	  std::queue<T> queue_;
+	  mutable boost::mutex mutex_;
+	  boost::condition condition_;
+
+	public:
+
+	  Queue(int timeout=0);
+
+	  void push(const T& data);
+
+	  bool empty() const;
+
+	  T& front();
+
+	  T const& front() const;
+
+	  void wait();
+
+	  void timed_wait(const boost::xtime& xtime);
+
+	  void pop();
+      };
+
+  }
+}
+
+#endif
---
+++ source/lib/network/Transfer.h
@@ -0,0 +1,51 @@
+#ifndef BLITZORTUNG_NETWORK_TRANSFER_H_
+#define BLITZORTUNG_NETWORK_TRANSFER_H_
+
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <queue>
+
+#include <boost/thread/thread.hpp>
+#include <boost/thread/xtime.hpp>
+#include <boost/thread/condition.hpp>
+
+#include "data/sample/Base.h"
+#include "network/Queue.h"
+#include "network/Creds.h"
+
+namespace blitzortung {
+  namespace network {
+
+    class Transfer {
+      private:
+	int sleepTime_;
+	Queue<data::sample::Base*>& sampleQueue_;
+	const Creds& creds_;
+	data::sample::Base::VP samples_;
+	double eventRateLimit_;
+
+      public:
+	Transfer(Queue<bo::data::sample::Base*>& sampleQueue, const Creds& creds);
+
+	virtual ~Transfer();
+
+	//! set sleep time between data transfers in seconds
+	void setSleepTime(const int sleepTime);
+
+	//! set limit of average number of events per minute transmitted
+	void setEventRateLimit(const double eventRateLimit);
+
+	//! initialize network connection to server
+	int open_connection ();
+
+	//! get string to be transmitted for every sample
+	std::string sampleToString(const data::sample::Base& sample);
+
+	//! network transfer thread: wait for new samples and send them from time to time
+	void operator ()();
+    };
+  }
+}
+
+#endif
