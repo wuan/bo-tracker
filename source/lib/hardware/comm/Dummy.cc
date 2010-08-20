c5da4ac0299de0e130ab14643e0b3672de532a43
awuerl@gmx.net <>
2010-08-20 13:57:04 +0200

    missing files

---
+++ source/lib/hardware/comm/Dummy.cc
@@ -0,0 +1,48 @@
+#include <sstream>
+
+#include "namespaces.h"
+#include "exception/Base.h"
+#include "hardware/comm/Dummy.h"
+
+namespace blitzortung {
+  namespace hardware {
+    namespace comm {
+
+      Dummy::Dummy()
+      {
+      }
+
+      Dummy::~Dummy() {
+      }
+
+      bool Dummy::isOpen() const {
+	return readPosition_ < dummyData_.size();
+      }
+
+      void Dummy::setBaudRate(const unsigned int baudRate) {
+	dummyBaud_ = baudRate;
+      }
+
+      const unsigned int Dummy::getBaudRate() const {
+	return dummyBaud_;
+      }
+
+      std::string Dummy::receive() {
+	if (isOpen())
+	  return dummyData_[readPosition_++];
+
+	return "";
+      }
+
+      void Dummy::send(const std::string& data) {
+	if (logger_.isInfoEnabled())
+	  logger_.infoStream() << "send() '" << data << "'";
+      }
+      
+      void Dummy::addReceivedLine(const std::string& line) {
+	dummyData_.push_back(line+"\r");
+      }
+
+    }
+  }
+}
