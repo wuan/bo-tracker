dee501941e8183619602ea0ddc77413a0ffc40fd
awuerl@gmx.net <>
2010-08-25 00:35:20 +0200

    added automatic checksum and command generation in serial comm

--- source/lib/hardware/gps/Sirf.cc
+++ source/lib/hardware/gps/Sirf.cc
@@ -23,29 +23,29 @@ namespace blitzortung {
 	  logger_.debugStream() << "initWrite() " << targetBaudRate;
 
 	// enable 1 HZ GCA message
-	communication_.send("$PSRF103,00,00,01,01*25\n");
+	communication_.send("PSRF103,00,00,01,01");
 
 	// disable GLL message '
-	communication_.send("$PSRF103,01,00,00,01*25\n");
+	communication_.send("PSRF103,01,00,00,01");
 
 	// disable GSA message '
-	communication_.send("$PSRF103,02,00,00,01*26\n");
+	communication_.send("PSRF103,02,00,00,01");
 
 	// disable GSV message '
-	communication_.send("$PSRF103,03,00,00,01*27\n");
+	communication_.send("PSRF103,03,00,00,01");
 
 	// enable 1 HZ RMC message '
-	communication_.send("$PSRF103,04,00,01,01*21\n");
+	communication_.send("PSRF103,04,00,01,01");
 
 	// disable VTG message '
-	communication_.send("$PSRF103,05,00,00,01*21\n");
-	//communication_.send("$PSRF103,06,00,00,01*22\r\n");
-	//communication_.send("$PSRF103,08,00,00,01*2C\r\n");
+	communication_.send("PSRF103,05,00,00,01");
+	//communication_.send("PSRF103,06,00,00,01");
+	//communication_.send("PSRF103,08,00,00,01");
 
 	// enable NMEA protocol, baud rate, 8 data bits, 1 stop bit, no parity '
 	{
 	  std::ostringstream oss;
-	  oss << "$PSRF100,1," << targetBaudRate << ",8,1,0*38\n";
+	  oss << "PSRF100,1," << targetBaudRate << ",8,1,0";
 	  communication_.send(oss.str());
 	}
       }
