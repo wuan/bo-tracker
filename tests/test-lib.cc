0940424409396ac2df37ced0c6368dc3d2dd3dfa
Andreas Wuerl <andi@tryb.de>
2010-09-14 14:30:11 +0000

    added debug mode for test

--- tests/test-lib.cc
+++ tests/test-lib.cc
@@ -1,3 +1,11 @@
+/*  
+
+    Copyright (C) 2010       Andreas Würl <awuerl@gmx.net>
+
+*/
+
+#include <boost/program_options.hpp>
+
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/CompilerOutputter.h>
@@ -13,13 +21,44 @@
 
 int main (int argc, char* argv[])
 {
+  // programm arguments/options
+  boost::program_options::options_description desc("program options");
+  desc.add_options()
+    ("help,h", "show program help")
+    ("verbose,v", "verbose mode")
+    ("debug,d", "debug mode")
+    ;
+
+
+  // parse command line options
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+  po::notify(vm); 
+
+  // help output or no 'sql-statement' given
+  if (vm.count("help")) {
+    std::cout << argv[0] << " [options]" << std::endl << std::endl;
+    std::cout << desc << std::endl;
+    std::cout << std::endl;
+
+    return 1;
+  }
+
   // Informiert Test-Listener ueber Testresultate
   bo::Logger logger;
 
   logger.setPriority(log4cpp::Priority::NOTICE);
-  //logger.setPriority(log4cpp::Priority::DEBUG);
+
+  if (vm.count("verbose")) {
+    logger.setPriority(log4cpp::Priority::INFO);
+  }
+
+  if (vm.count("debug")) {
+    logger.setPriority(log4cpp::Priority::DEBUG);
+  }
 
 
+  // create test result object
   CPPUNIT_NS::TestResult testResult;
 
   // Listener zum Sammeln der Testergebnisse registrieren
