6ee12ff5078729e9825283a287784bdf25709e1f
Andreas Wuerl <andi@tryb.de>
2010-09-18 15:54:46 +0200

    added documentation to data::Waveform

--- source/lib/data/Waveform.h
+++ source/lib/data/Waveform.h
@@ -15,25 +15,46 @@ namespace blitzortung {
 
       private:
 
+	//! reference time
 	const pt::ptime t0_;
+	
+	//! maximum absolute value of signal
 	float maxVal_;
+	
+	//! index of maximum absolute value of signal
 	int maxIndex_;
+	
+	//! vector for x channel data
 	std::vector<T> xdata_;
+	
+	//! vector for y channel data
 	std::vector<T> ydata_;
 
       public:
 
-	Waveform(const pt::ptime&);
+        //! create a waveform object
+	/*!
+	\param t0 reference time of waveform
+	*/
+	Waveform(const pt::ptime& t0);
 
+	//! delete waveform object
 	virtual ~Waveform();
 
 	//! add sample to collection
-	void add(T, T);
+	/*!
+	\param x x-value of sample
+	\param y y-value of sample
+	*/
+	void add(T x, T y);
 
+	//! returns index of maximum value
 	int getMaxIndex() const;
 
+	//! returns x-value at maximum
 	T getMaxX() const;
 
+	//! returns y-value at maximum
 	T getMaxY() const;
 
     };
