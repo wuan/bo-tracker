3a81615a2fad5ad6815fe35edb62d5bf22ad2d87
Andreas Wuerl <andi@tryb.de>
2011-03-12 08:22:10 +0100

    improved logging behaviour for ipc

--- source/lib/ipc/UnixSocket.cc
+++ source/lib/ipc/UnixSocket.cc
@@ -8,21 +8,29 @@ namespace blitzortung {
   namespace ipc {
 
     UnixSocket::UnixSocket(const std::string& socketFileName) :
+      socketFileName_(socketFileName), 
       logger_("UnixSocket")
     {
       socket_ = socket(AF_UNIX, SOCK_STREAM, 0);
 
-      logger_.infoStream() << "create unix domain socket " << socket_;
+      if (logger_.isDebugEnabled())
+	logger_.debugStream() << "create unix domain socket " << socket_;
+
       sockaddr_un sockaddr;
       sockaddr.sun_family = AF_UNIX;
-      strcpy(sockaddr.sun_path, "/tmp/mysocket");
+
+      if (logger_.isDebugEnabled())
+	logger_.debugStream() << "bind to socket file '" << socketFileName << "'";
+
+      strcpy(sockaddr.sun_path, socketFileName.c_str());
+
       unlink(sockaddr.sun_path);
       int addrlen = sizeof(sockaddr.sun_family) + strlen(sockaddr.sun_path); 
+
       bind(socket_, (struct sockaddr *)&sockaddr, addrlen);
 
       Listener listener(socket_, (struct sockaddr*)&sockaddr, addrlen);
 
-      // start listener thread
       boost::thread thread(listener);
     }
 
@@ -30,7 +38,9 @@ namespace blitzortung {
     UnixSocket::~UnixSocket()
     {
       close(socket_);
-      logger_.infoStream() << "close socket " << socket_;
+      unlink(socketFileName_.c_str());
+      if (logger_.isDebugEnabled())
+	logger_.debugStream() << "close socket " << socket_;
     }
 
   }
