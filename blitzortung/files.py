89ad0d50d7800fe99328fdcc785931e9aea7244c
Andreas Wuerl <andi@debian>
2011-05-15 19:19:21 +0200

    now using cdbs for python packaging fixed errors in python module

--- blitzortung/files.py
+++ blitzortung/files.py
@@ -12,60 +12,59 @@ import datetime
 
 class RawFiles(object):
 
-    def __init__(self, rawPath):
-        rawFileNames = glob.glob(os.path.join(rawPath, '*.bor'))
+  def __init__(self, rawPath):
+    rawFileNames = glob.glob(os.path.join(rawPath, '*.bor'))
 
-        rawFileNames.sort()
+    rawFileNames.sort()
 
-        self.rawFiles = {}
+    self.rawFiles = {}
 
-        for rawFileName in rawFileNames:
-            date = datetime.datetime.strptime(rawFileName[-12:-4], '%Y%m%d').date()
-            if not self.rawFiles.has_key(date):
-                self.rawFiles[date] = rawFileName
-            else:
-                raise Exception("ERROR: double date! " + rawFileName + " vs. " + self.rawFiles[date])
+    for rawFileName in rawFileNames:
+      date = datetime.datetime.strptime(rawFileName[-12:-4], '%Y%m%d').date()
+      if not self.rawFiles.has_key(date):
+        self.rawFiles[date] = rawFileName
+      else:
+        raise Exception("ERROR: double date! " + rawFileName + " vs. " + self.rawFiles[date])
 
-    def get(self, date):
-        if self.rawFiles.has_key(date):
-            return self.rawFiles[date]
-        else:
-            raise Exception("no file for date "+date.strftime('%Y-%m-%d'))
+  def get(self, date):
+    if self.rawFiles.has_key(date):
+      return self.rawFiles[date]
+    else:
+      raise Exception("no file for date "+date.strftime('%Y-%m-%d'))
 
 
 class Data(object):
 
-    def __init__(self, files, time):
-        self.files = files
-        self.time = time
+  def __init__(self, files, time):
+    self.files = files
+    self.time = time
 
-    def get(self):
-        start = self.time.getStartTime()
-        starttime = start.strftime("%H%M")
-        startdate = start.strftime("%Y%m%d")
+  def get(self):
+    start = self.time.getStartTime()
+    starttime = start.strftime("%H%M")
+    startdate = start.strftime("%Y%m%d")
 
-        end = self.time.getEndMinute()
-        endtime = end.strftime("%H%M")
+    end = self.time.getEndMinute()
+    endtime = end.strftime("%H%M")
 
-        try:
-            rawFile = files.get(start.date())
+    try:
+      rawFile = files.get(start.date())
 
-            data = subprocess.Popen(['blitzortung-data','-i', rawFile, '-s', starttime, '-e', endtime], stdout=subprocess.PIPE)
+      data = subprocess.Popen(['blitzortung-data','-i', rawFile, '-s', starttime, '-e', endtime], stdout=subprocess.PIPE)
 
-            (output, error) = data.communicate()
-        except:
-            pass
-            output = ""
-            print "ignore errors"
+      (output, error) = data.communicate()
+    except:
+      pass
+      output = ""
+      print "ignore errors"
 
-	rawEvents = []
+    rawEvents = []
 
-	for line in output.splitlines():
-	  rawEvents.append(RawEvent(line))
+    for line in output.splitlines():
+      rawEvents.append(RawEvent(line))
 
-	return rawEvents
+    return rawEvents
 
-    def list(self):
-        for event in self.get():
-            print event
-
+  def list(self):
+    for event in self.get():
+      print event
--- blitzortung/geom.py
+++ blitzortung/geom.py
@@ -10,20 +10,19 @@ import re
 
 class Point(object):
 
-    __whitespaceRe = re.compile('\s+')
+  __whitespaceRe = re.compile('\s+')
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
 
-    def __invgeod(self, other):
-        pipe = subprocess.Popen(['invgeod', '+ellps=WGS84', '+units=m','-f','%.5f'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-        (output, error) = pipe.communicate("%f %f %f %f" %(self.y, self.x, other.y, other.x))
-        return Point.__whitespaceRe.split(output.strip())
+  def __invgeod(self, other):
+    pipe = subprocess.Popen(['invgeod', '+ellps=WGS84', '+units=m','-f','%.5f'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+    (output, error) = pipe.communicate("%f %f %f %f" %(self.y, self.x, other.y, other.x))
+    return Point.__whitespaceRe.split(output.strip())
 
-    def distance(self, other):
-        return float(self.__invgeod(other)[2])
+  def distance(self, other):
+    return float(self.__invgeod(other)[2])
 
-    def azimuth(self, other):
-        return float(self.__invgeod(other)[0])
-
+  def azimuth(self, other):
+    return float(self.__invgeod(other)[0])
