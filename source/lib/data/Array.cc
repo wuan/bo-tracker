70ea6aee48f4569aa8c4c877198dbdb571c51223
Andreas Wuerl <andi@debian>
2012-05-02 07:38:40 +0200

    separate amplitude square method

--- source/lib/data/Array.cc
+++ source/lib/data/Array.cc
@@ -6,20 +6,24 @@ namespace blitzortung {
     Array::~Array() = default;
 
     float Array::getAmplitude(unsigned int index) const {
-      double sum = 0.0;
-      unsigned short channels = getNumberOfChannels();
-
-      if (channels > 1) {
-	for (int channel = 0; channel < channels; channel++) {
-	  float amp = getFloat(index, channel);
-	  sum += amp * amp;
-	}
-
-	return sqrt(sum);
+      if (getNumberOfChannels() > 1) {
+	return sqrt(getAmplitudeSquare(index));
       } else {
 	return getFloat(index, 0);
       }
     }
+
+    float Array::getAmplitudeSquare(unsigned int index) const {
+      double sum = 0.0;
+
+	for (int channel = 0; channel < getNumberOfChannels(); channel++) {
+	  float amp = getFloat(index, channel);
+	  sum += amp * amp;
+	}
+
+	return sum;
+    }
+
 
     float Array::getPhase(unsigned int index) const {
 
@@ -36,8 +40,9 @@ namespace blitzortung {
     unsigned int Array::getMaxIndex() const {
       unsigned int maxIndex = 0;
       float maxAmplitude = 0.0;
+
       for (unsigned int sample=0; sample < getNumberOfSamples(); sample++) {
-	double amplitude = getAmplitude(sample);
+	double amplitude = getAmplitudeSquare(sample);
 	if (amplitude > maxAmplitude) {
 	  maxIndex = sample;
 	  maxAmplitude = amplitude;
