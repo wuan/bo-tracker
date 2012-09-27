52a551c9c2d5124020cc883ab5c57b4e42d2d346
Andreas Wuerl <andi@tryb.de>
2012-09-27 22:23:01 +0000

    prepared 1.0.6 release

--- source/lib/hardware/Pcb.cc
+++ source/lib/hardware/Pcb.cc
@@ -27,7 +27,7 @@ namespace blitzortung {
 	logger_.debugStream() << "destroyed";
     }
 
-    const std::string Pcb::version_ = "debT&nbsp;1.0.5";
+    const std::string Pcb::version_ = "debT&nbsp;1.0.6";
 
     bool Pcb::isOpen() const {
       return comm_.isOpen();
